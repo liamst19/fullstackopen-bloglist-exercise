510c5e96fe83777c3f3bae0f738d134a4b192071
liamst19 <50284753+liamst19@users.noreply.github.com>
2020-04-06 20:17:12 +0900

    update packages

--- bloglist-frontend/src/components/UsersList.test.js
+++ bloglist-frontend/src/components/UsersList.test.js
@@ -7,25 +7,15 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import '@testing-library/jest-dom/extend-expect'
 import { render, cleanup } from '@testing-library/react'
 
-<<<<<<< HEAD
 import userReducer from '../reducers/userReducer.js'
 jest.mock('../reducers/userReducer.js')
 
 import testHelper from '../tests/test_helper.js'
 
-=======
->>>>>>> fad1ea8... cypress setup
 import UsersList from './UsersList'
 
 describe('UsersList component', () => {
 
-<<<<<<< HEAD
-  const testReducer = combineReducers({
-    users: userReducer
-  })
-
-  const testStore = createStore(testReducer, applyMiddleware(thunk))
-=======
   const usersData = [
     {
       id: 0,
@@ -64,7 +54,6 @@ describe('UsersList component', () => {
 
     return createStore(testReducer, applyMiddleware(thunk))
   }
->>>>>>> fad1ea8... cypress setup
 
   beforeEach(() => {
 
@@ -75,20 +64,13 @@ describe('UsersList component', () => {
   })
 
   test('component renders', () => {
-<<<<<<< HEAD
-    const component = render(<Provider store={testStore}>
-=======
     const component = render(<Provider store={testStore()}>
->>>>>>> fad1ea8... cypress setup
                               <Router>
                                 <UsersList />
                               </Router></Provider>)
     const tableRows = component.container.getElementsByTagName('tbody')[0].children
-<<<<<<< HEAD
     expect(tableRows.length).toBe(testHelper.usersData.length)
-=======
     expect(tableRows.length).toBe(usersData.length)
->>>>>>> fad1ea8... cypress setup
 
   })
 })
